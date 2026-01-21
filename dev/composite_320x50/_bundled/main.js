31d704b302a85b62a5f2b3a5987280c71f466072
Gar <gar@lonelydatum.com>
2026-01-21 16:42:17 -0500

    new logo

--- dev/composite_320x50/_bundled/main.js
+++ dev/composite_320x50/_bundled/main.js
@@ -105,53 +105,55 @@ exports.olg = olg;
 },{}],3:[function(require,module,exports){
 "use strict";
 
-var _commonJsCommonJs = require('../../_common/js/common.js');
+var _commonJsCommonJs = require("../../_common/js/common.js");
 
 var ypy = new TimelineMax();
-ypy.from([".ypy_1", ".ypy_2", ".ypy_3"], { duration: .3, y: "-=200", stagger: .13 });
+ypy.from([".ypy_1", ".ypy_2", ".ypy_3"], { duration: 0.3, y: "-=200", stagger: 0.13 });
 
 function init(_ref) {
-	var ypy = _ref.ypy;
-	var device = _ref.device;
+  var ypy = _ref.ypy;
+  var device = _ref.device;
 
-	var tl = new TimelineMax({ onComplete: function onComplete() {
-			if (document.getElementById("legalBtn")) {
-				TweenLite.set("#legalBtn", { display: "block" });
-			}
-		} });
+  var tl = new TimelineMax({
+    onComplete: function onComplete() {
+      if (document.getElementById("legalBtn")) {
+        TweenLite.set("#legalBtn", { display: "block" });
+      }
+    }
+  });
 
-	TweenLite.to(".hero_on", { duration: 2, opacity: 1, yoyo: true, repeat: 0, repeatDelay: 0, ease: "back.out" });
+  TweenLite.to(".hero_on", { duration: 2, opacity: 1, yoyo: true, repeat: 0, repeatDelay: 0, ease: "back.out" });
 
-	TweenLite.to(".phone", { duration: .8, opacity: .6, yoyo: true, repeat: 11, repeatDelay: 0, ease: "back.out" });
+  TweenLite.to(".phone", { duration: 0.8, opacity: 0.6, yoyo: true, repeat: 11, repeatDelay: 0, ease: "back.out" });
 
-	tl.set(".frame1", { opacity: 1 });
-	tl.set(".end_device", { opacity: 0 });
+  tl.set(".frame1", { opacity: 1 });
+  tl.set(".end_device", { opacity: 0 });
 
-	tl.add(ypy);
+  tl.add(ypy);
 
-	tl.to(".ypy", { duration: .3, opacity: 0 }, "+=1");
+  tl.to(".ypy", { duration: 0.3, opacity: 0 }, "+=1");
 
-	tl.add("t1", "+=.2");
-	tl.from([".t1"], { duration: .3, y: "+=30", opacity: 0 }, "t1");
+  tl.add("t1", "+=.2");
+  tl.from([".t1"], { duration: 0.3, y: "+=30", opacity: 0 }, "t1");
 
-	tl.to(".t1", { duration: .3, opacity: 0 }, "+=" + _commonJsCommonJs.read.t1);
+  tl.to(".t1", { duration: 0.3, opacity: 0 }, "+=" + _commonJsCommonJs.read.t1);
 
-	tl.add("t2");
+  tl.add("t2");
 
-	tl.from(".t2", { duration: .3, opacity: 0 }, "t2");
-	tl.to(".t2", { duration: .3, opacity: 0 }, "+=" + _commonJsCommonJs.read.t2);
+  tl.from(".t2", { duration: 0.3, opacity: 0 }, "t2");
+  tl.to(".t2", { duration: 0.3, opacity: 0 }, "+=" + _commonJsCommonJs.read.t2);
 
-	// tl.to([  ".frame1"], {duration:.3, opacity:0} )
+  // tl.to([  ".frame1"], {duration:.3, opacity:0} )
 
-	tl.set(".frame2", { opacity: 1 }, "+=.3");
+  tl.set(".frame2", { opacity: 1 }, "+=.3");
 
-	tl.from([".device"], { duration: .5, opacity: 0 });
-	tl.from(".end_url", { duration: .3, opacity: 0 }, "+=.3");
-	tl.from(".end_ypy", { duration: .3, opacity: 0 }, "+=.3");
-	tl.from(".end_cta", { duration: .3, opacity: 0, y: "+=50", opacity: 0 }, "+=.3");
-
-	tl.add((0, _commonJsCommonJs.olg)());
-	return tl;
+  tl.from([".device"], { duration: 0.5, opacity: 0 });
+  tl.from(".end_url", { duration: 0.3, opacity: 0 }, "+=.3");
+  tl.from(".end_ypy", { duration: 0.3, opacity: 0 }, "+=.3");
+  tl.from(".end_cta", { duration: 0.3, opacity: 0, y: "+=50", opacity: 0 }, "+=.3");
+  tl.to("._logo", { duration: 0.3, y: "+=90" });
+  tl.add((0, _commonJsCommonJs.olg)());
+  return tl;
 }
 
 init({ ypy: ypy });
